2d11a42af69f119c163f96d5959da7e7914e0db6
Jeff Lapp <lappjeff20@gmail.com>
2019-09-19 18:30:17 -0400

    Completed BFS function

--- binary_search_tree/binary_search_tree.py
+++ binary_search_tree/binary_search_tree.py
@@ -1,3 +1,8 @@
+import sys
+sys.path.append('../queue_and_stack')
+from dll_queue import Queue
+from dll_stack import Stack
+
 class BinarySearchTree:
       def __init__(self, value):
         self.value = value
@@ -44,3 +49,52 @@ class BinarySearchTree:
               self.left.for_each(cb)
           if self.right:
               self.right.for_each(cb)
+
+      def in_order_print(self):
+          pass
+
+      # Print the value of every node, starting with the given node,
+      # in an iterative breadth first traversal
+      def bft_print(self, node):
+          queue = Queue()
+          queue.enqueue(self)
+
+          current_node = None
+
+          while queue.len() > 0:
+
+              current_node = queue.dequeue()
+
+              if current_node.left:
+                  queue.enqueue(current_node.left)
+              if current_node.right:
+                  queue.enqueue(current_node.right)
+
+              print(current_node.value)
+
+      # Print the value of every node, starting with the given node,
+      # in an iterative depth first traversal
+      def dft_print(self, node):
+          # '''create a stack
+          # put root in stack
+          # while stack is not empty
+          # pop first item in stack
+          # check root.left and put it in stack
+          # check root.right and put it in stack
+          # go to top of stack and continue'''
+          stack = Stack()
+
+      def pre_order_dft(self, node):
+          pass
+
+      def post_order_dft(self, node):
+          pass
+
+bst = BinarySearchTree(1)
+bst.insert(8)
+bst.insert(5)
+bst.insert(7)
+bst.insert(6)
+bst.insert(3)
+bst.insert(4)
+bst.insert(2)
